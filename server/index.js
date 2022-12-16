575ca53174d33a61fb720e5d92757a13da49317c
faint <ifaintsmile@gmail.com>
2022-12-16 03:09:04 -0800

    Renamed app.js to index.js. Added vercel.json file

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ require('./config/passport')
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
